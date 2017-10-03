c198fc7af63aaf42579b51541c1e582fda328239
= <=>
2017-10-03 14:30:09 +0700

    Publihs

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+;(function($) {
+
+   'use strict'
+
+	var tabs = function() {
+		$('#tabs').each(function() {
+
+			$(this).find('.tab-nav li').first().addClass('active');
+			$(this).children('.tab-content').first().addClass('active').show();
+
+			//
+			$(this).find('.tab-nav li').on('click',function() {
+				
+				var tabNavIndex = $(this).index();
+				var conIndex = $(this).parents('#tabs').children('.tab-content').eq(tabNavIndex);
+
+				$(this).addClass('active').siblings().removeClass('active');
+				conIndex.addClass('active').siblings('.tab-content').removeClass('active');
+			});
+
+		});
+	};
+
+	// 
+	$(function($) {
+		tabs();
+	});
+
+
+})(jQuery); 
