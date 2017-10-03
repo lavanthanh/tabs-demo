2a6caa3203e34fa8f7fd94ff4e3c0fc29f082dd6
= <=>
2017-10-03 14:46:20 +0700

    Update content

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
    'use strict'
 
 	var tabs = function() {
-		$('#tabs').each(function() {
+		$('.tabs').each(function() {
 
 			$(this).find('.tab-nav li').first().addClass('active');
 			$(this).children('.tab-content').first().addClass('active').show();
@@ -12,7 +12,7 @@
 			$(this).find('.tab-nav li').on('click',function() {
 				
 				var tabNavIndex = $(this).index();
-				var conIndex = $(this).parents('#tabs').children('.tab-content').eq(tabNavIndex);
+				var conIndex = $(this).parents('.tabs').children('.tab-content').eq(tabNavIndex);
 
 				$(this).addClass('active').siblings().removeClass('active');
 				conIndex.addClass('active').siblings('.tab-content').removeClass('active');
